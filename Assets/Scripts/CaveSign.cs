099f110c7b75c8afd44f144a3be14a716912e903
James Cook <james@trainerroad.com>
2022-08-21 19:51:36 -0700

    random update to sign

--- Assets/Scripts/CaveSign.cs
+++ Assets/Scripts/CaveSign.cs
@@ -5,9 +5,7 @@ public class CaveSign : MonoBehaviour, ISign
 {
     public List<string> text { get; set; } = new List<string>
     {
-        "Entrance to a mysterious cave. Don't worry it's not scary or anything.",
-        "Enter at your own risk...",
-        "Oh shoot, that sounded scary.",
-        "Just go in... Or don't. I don't get paid enough for this."
+        "6 million ways to die",
+        "Choose one",
     };
 }
