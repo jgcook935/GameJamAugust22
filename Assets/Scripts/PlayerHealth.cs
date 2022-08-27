339aaa6810461e681690f224e0da72fc1f172e2b
James Cook <james@trainerroad.com>
2022-08-27 13:59:27 -0700

    flash the character when he gets hit, disable box collider while flashing

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,8 +1,17 @@
 using UnityEngine;
+using System.Collections;
 
 public class PlayerHealth : MonoBehaviour
 {
     public float health = 200f;
+    public SpriteRenderer spriteRenderer;
+    public BoxCollider2D boxCollider;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -10,10 +19,23 @@ public class PlayerHealth : MonoBehaviour
         {
             if (collision.otherCollider.CompareTag("Player"))
             {
-                Debug.Log($"Player was touched by the enemy - DO DAMAGE STUFF HERE");
+                StartCoroutine(FlashSprite());
             }
             var damage = collision.gameObject.GetComponent<EnemyHealth>().damage;
             health -= damage;
         }
+    }
+
+    IEnumerator FlashSprite()
+    {
+        boxCollider.enabled = false;
+        for (int i = 0; i < 6; i++)
+        {
+            spriteRenderer.enabled = false;
+            yield return new WaitForSeconds(.25f);
+            spriteRenderer.enabled = true;
+            yield return new WaitForSeconds(.25f);
+        }
+        boxCollider.enabled = true;
     }
 }
