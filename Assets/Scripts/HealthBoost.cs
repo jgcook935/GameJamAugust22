dae3f78d66fb2a43800edf531b64b4d4931ebe0f
James Cook <james@trainerroad.com>
2022-09-16 14:56:26 -0700

    eating sound for health boost

--- Assets/Scripts/HealthBoost.cs
+++ Assets/Scripts/HealthBoost.cs
@@ -1,11 +1,24 @@
+using System.Collections;
 using UnityEngine;
 
 public class HealthBoost : MonoBehaviour
 {
+    AudioSource source => GetComponent<AudioSource>();
+    SpriteRenderer sr => GetComponent<SpriteRenderer>();
+
     void OnTriggerEnter2D()
     {
+        source.Play();
+        sr.enabled = false;
         PlayerHealth.Instance.soHealth.Value = PlayerHealth.Instance.maxHealth;
         Healthbar.Instance.UpdateHealthBar();
-        Destroy(this.gameObject);
+        StartCoroutine(DestroyAfter(gameObject));
+    }
+
+    IEnumerator DestroyAfter(GameObject someObject)
+    {
+        yield return new WaitForSeconds(2f);
+        Destroy(someObject);
+        CharacterManager.Instance.SetControlsEnabled(true);
     }
 }
